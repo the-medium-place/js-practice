54f3ea897ea1be14e7f0dea32bd87d638bf50345
ZGS <zgstowell@gmail.com>
2020-09-08 15:04:06 -0700

    nested array practice

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
-var nameArray = ['zac', 'darth vader', 'goku', 'homer', 'bertha', 'joe rehfuss', 'joe rehfuss']
+// var nameArray = ['zac', 'darth vader', 'goku', 'homer', 'bertha', 'joe rehfuss', 'joe rehfuss']
 // var userInput = prompt("Search for a student: ");
-console.log(nameArray);
 
-var newStudent = prompt("Enter the new Name");
+// var newArray = [['zac', 'goku', 'joe'], ['mike', 'scotty', 'magic']]
 
-nameArray.push(newStudent);
+// console.log(newArray[0][1])
 
-console.log(nameArray);
+// for (let i = 0; i < newArray.length; i++) {
+//     for (let j = 0; j < newArray[i].length; j++) {
+//         console.log(newArray[i][j])
+//     }   
+// }
 
-var numberOfGokus = 0;
-var numberOfJoes = 0; 
+// console.log(nameArray);
 
-for (let i = 0; i < nameArray.length; i++) {
-    if(nameArray[i] === 'goku'){
-        console.log("found one!");
-        numberOfGokus++
-    }  
-}
+// var newStudent = prompt("Enter the new Name");
 
-for (let i = 0; i < nameArray.length; i++) {
-    if(nameArray[i] === 'joe rehfuss'){
-        console.log("found joe!");
-        numberOfJoes++
-    }  
-}
+// nameArray.push(newStudent);
 
-if(numberOfGokus > numberOfJoes){
-    console.log("not a fair fight");
-} else if (numberOfJoes > numberOfGokus){
-    console.log("joe's gonna win every time");
-} else {
-    console.log("this seems pretty fair.")
-}
+// console.log(nameArray);
+
+// var numberOfGokus = 0;
+// var numberOfJoes = 0; 
+
+// for (let i = 0; i < nameArray.length; i++) {
+//     if(nameArray[i] === 'goku'){
+//         console.log("found one!");
+//         numberOfGokus++
+//     }  
+
+//     if(nameArray[i] === 'joe rehfuss'){
+//         console.log("found joe!");
+//         numberOfJoes++
+//     }  
+// }
+
+// if(numberOfGokus > numberOfJoes){
+//     console.log("not a fair fight");
+// } else if (numberOfJoes > numberOfGokus){
+//     console.log("joe's gonna win every time");
+// } else {
+//     console.log("this seems pretty fair.")
+// }
 
 
 
 
-console.log(numberOfGokus)
+// console.log(numberOfGokus)
 
 
 
 
 // console.log(userInput)
+
+
