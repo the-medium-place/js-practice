bd701fd22eb376347f1c68581c7e227fede0fb97
ZGS <zgstowell@gmail.com>
2020-09-08 14:53:25 -0700

    added some user input

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var nameArray = ['zac', 'darth vader', 'goku', 'homer', 'bertha', 'joe rehfuss', 'joe rehfuss']
 // var userInput = prompt("Search for a student: ");
+console.log(nameArray);
+
+var newStudent = prompt("Enter the new Name");
+
+nameArray.push(newStudent);
+
+console.log(nameArray);
 
 var numberOfGokus = 0;
 var numberOfJoes = 0; 
-
 
 for (let i = 0; i < nameArray.length; i++) {
     if(nameArray[i] === 'goku'){
